be5f982f34c70e93a066a089fda47d4a6ee2e1fa
Abdelrahman Yasser <112268441+Abdelrahman-ammar@users.noreply.github.com>
2024-10-05 00:59:05 +0300

    Add Ai container

---
+++ Deployment/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c30ba9b0374526394200f5b0b19c2f27f1cf369e639701ab95ef11a02968e2a
+size 1418
---
+++ Deployment/preprocessing.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4123887d3d63d4f8eada8bd5b47b62cffc24a895741dad3de216c0608afa532b
+size 2371
